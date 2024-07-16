17e8ad2960c412bbff0e983ffef71054663e8540
Ravi G <ravg1@publicisgroupe.net>
2024-07-16 09:23:20 +0530

    Config and new test added1

--- src/test/java/Selenium4/TestGridInDocker.java
+++ src/test/java/Selenium4/TestGridInDocker.java
@@ -5,6 +5,7 @@ import java.net.URL;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
@@ -19,7 +20,6 @@ public class TestGridInDocker {
 //		firefoxRun();
 //		edgeRun();
 	}
-	@Test
 	private static void chromeRun() throws MalformedURLException, InterruptedException {
 		ChromeOptions cOptions = new ChromeOptions();
 		WebDriver cDriver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),cOptions);
@@ -29,7 +29,6 @@ public class TestGridInDocker {
 		cDriver.close();
 		System.out.println("=====   GRID CHROME STANDALONE COMPLETED =====");
 	}
-	@Test
 	private static void firefoxRun() throws MalformedURLException, InterruptedException {
 		FirefoxOptions fOptions =  new FirefoxOptions();
 		WebDriver fDriver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),fOptions);
@@ -50,4 +49,14 @@ public class TestGridInDocker {
 		System.out.println("=====   GRID EDGE STANDALONE COMPLETED =====");
 	}
 	
+	@Test
+	private static void JustChromeExecution() throws InterruptedException {
+			WebDriver cDriver = new ChromeDriver();
+			cDriver.get("https://www.selenium.dev/documentation/");
+			cDriver.findElement(By.xpath("//*[@id=\"m-documentationgrid\"]/span")).click();
+			Thread.sleep(2000);
+			cDriver.close();
+			System.out.println("=====   GRID CHROME STANDALONE COMPLETED =====");
+	}
+
 }
